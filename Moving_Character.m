a1842ef72dec239e353a3a9ef337c6a405fed930
lokin7778 <lokin7778@gmail.com>
2025-12-01 10:21:47 -0500

    Added the borders for the entire moving characters function page's scene

--- Moving_Character.m
+++ Moving_Character.m
@@ -2,9 +2,35 @@ function score_moving = Moving_Character(level)
 
     % The protagonist sprite 
     % Create the scene
+
+    border_outline_topleft = 130;
+    border_outline_topright = 132;
+    border_outline_bottomleft = 194;
+    border_outline_bottomright = 196;
+    border_outline_left = 162;
+    border_outline_right = 164;
+    border_top_top = 131;
+    border_bottom_bottom = 195;
+    
     cinScene = simpleGameEngine("custom.png",16,16,5,[217, 135, 41]);
     displayBox = ones(12,20);
-    
+
+    border_top = [border_outline_topleft,repmat(border_top_top,1,18),border_outline_topright];
+    border_middle1 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle2 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle3 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle4 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle5 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle6 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle7 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle8 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle9 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_middle10 = [border_outline_left, repmat(1,1,18), border_outline_right];
+    border_bottom = [border_outline_bottomleft, repmat(border_bottom_bottom,1,18), border_outline_bottomright];
+
+    borders = [border_top; border_middle1; border_middle2; border_middle3; border_middle4; ...
+        border_middle5; border_middle6; border_middle7; border_middle8; border_middle9; border_middle10; border_bottom];
+
     question_type = randi([1,2]);
     [question, output1, output2, output3, output4, correct_english] = ranQuestion(1,10, question_type)
 
@@ -16,7 +42,7 @@ function score_moving = Moving_Character(level)
     j = 10;
     
     displayBox(i,j) = player_sprite;
-    drawScene(cinScene,displayBox);
+    drawScene(cinScene,displayBox,borders);
     
     % while 1 is an infinite while loop
     while 1
@@ -35,7 +61,7 @@ function score_moving = Moving_Character(level)
             text(50, 870, output3, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             text(1450, 870, output4, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             i = i-1;
-            drawScene(cinScene,displayBox);
+            drawScene(cinScene,displayBox, borders);
         elseif strcmp(key, 'downarrow')
             displayBox(i,j) = 1;
             displayBox(i+1,j) = player_sprite;
@@ -44,7 +70,7 @@ function score_moving = Moving_Character(level)
             text(50, 870, output3, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             text(1450, 870, output4, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             i = i+1;
-            drawScene(cinScene,displayBox);
+            drawScene(cinScene,displayBox, borders);
         elseif strcmp(key, 'leftarrow')
             displayBox(i,j) = 1;
             displayBox(i,j-1) = player_sprite;
@@ -53,7 +79,7 @@ function score_moving = Moving_Character(level)
             text(50, 870, output3, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             text(1450, 870, output4, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             j = j-1;
-            drawScene(cinScene,displayBox);
+            drawScene(cinScene,displayBox, borders);
         elseif strcmp(key, 'rightarrow')
             displayBox(i,j) = 1;
             displayBox(i,j+1) = player_sprite;
@@ -62,7 +88,7 @@ function score_moving = Moving_Character(level)
             text(50, 870, output3, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             text(1450, 870, output4, 'FontSize', 25, 'Color',[0.8 0.8 0.8]); % hiragana
             j = j+1;
-            drawScene(cinScene,displayBox);
+            drawScene(cinScene,displayBox, borders);
         end
     end
 
